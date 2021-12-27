2032fc99214f07c68d70391f8b603b77995894ff
Luvesh L Sadhwani <luveshsadhwani@gmail.com>
2021-12-27 12:59:46 +0800

    ADD axios api for logout

--- src/pages/DashboardPage/index.tsx
+++ src/pages/DashboardPage/index.tsx
@@ -1,7 +1,5 @@
 import {
-    IonBackButton,
   IonButton,
-  IonButtons,
   IonCol,
   IonContent,
   IonGrid,
@@ -13,19 +11,48 @@ import {
 } from "@ionic/react";
 import React from "react";
 import { RouteComponentProps } from "react-router";
+import axios from "axios";
 
 const DashboardPage: React.FC<RouteComponentProps> = ({ history }) => {
-  const handleLogout = () => {
-    history.push("/login");
+  const getJwtFromStorage = () => {
+    return localStorage.getItem("token");
+  };
+
+  const deleteJwtFromStorage = () => {
+    return localStorage.removeItem("token");
+  };
+
+  const handleLogout = async () => {
+    try {
+      const token = getJwtFromStorage();
+      const response = await axios({
+        method: "post",
+        url: "http://127.0.0.1:8000/api/logout",
+        data: {},
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        validateStatus: function (status) {
+          return status === 200; // default
+        },
+      });
+
+      const { status, data } = response;
+      // handle correct response
+      if (status === 200) {
+        deleteJwtFromStorage();
+        history.push("/login");
+        console.log(data);
+      }
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonButtons slot="start">
-            <IonBackButton />
-          </IonButtons>
           <IonTitle>Dashboard</IonTitle>
         </IonToolbar>
       </IonHeader>
